1def985b44e73b07467cb72810c75b246b365db8
martinkooij <martin.kooij.4@gmail.com>
2025-09-17 20:49:08 +0200

    chore: add some leds - use deep sleep

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -29,8 +29,14 @@ const SLID_IP: &str = "192.168.68.104";
 use esp_hal::{
     clock::CpuClock,
     delay::Delay,
+    gpio::{Io, Level, Output, OutputConfig, Pull},
     main,
     rng::Rng,
+    rtc_cntl::{
+        reset_reason,
+        sleep::{RtcioWakeupSource, TimerWakeupSource, WakeupLevel},
+        wakeup_cause, Rtc, SocResetReason,
+    },
     time::{self, Duration},
     timer::timg::TimerGroup,
 };
@@ -43,6 +49,7 @@ use smoltcp::{
     iface::{SocketSet, SocketStorage},
     wire::IpAddress,
 };
+
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
@@ -55,6 +62,24 @@ fn main() -> ! {
     esp_println::logger::init_logger(log::LevelFilter::Info);
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
+    let mut rtc = Rtc::new(peripherals.LPWR);
+    let mut red_led = Output::new(peripherals.GPIO8, Level::High, OutputConfig::default());
+    let mut led6 = Output::new(
+        peripherals.GPIO10,
+        Level::Low,
+        OutputConfig::default().with_pull(Pull::None),
+    );
+    led6.set_low();
+    let wake_reason = wakeup_cause();
+    match wake_reason {
+        esp_hal::system::SleepSource::Timer => {
+            println!("woke up from timer");
+            led6.set_high();
+        }
+        _ => {
+            println!("not a timer wakeup");
+        }
+    }
 
     esp_alloc::heap_allocator!(size: 72 * 1024);
 
@@ -69,7 +94,6 @@ fn main() -> ! {
 
     let mut sta_device = interfaces.sta;
     let sta_interface = create_interface(&mut sta_device);
-
     let now = || time::Instant::now().duration_since_epoch().as_millis();
 
     let mut sta_socket_set_entries: [SocketStorage; 3] = Default::default();
@@ -86,81 +110,97 @@ fn main() -> ! {
     let res = controller.set_configuration(&client_config);
     println!("wifi_set_configuration returned {:?}", res);
 
-    controller.start().unwrap();
-    println!("is wifi started: {:?}", controller.is_started());
-
-    println!("{:?}", controller.capabilities());
-
-    println!("wifi_connect {:?}", controller.connect());
-
-    // wait for STA getting an ip address
-    println!("Wait to get an ip address");
-    loop {
-        sta_stack.work();
-
-        if sta_stack.is_iface_up() {
-            println!("got ip {:?}", sta_stack.get_ip_info());
-            break;
-        }
-    }
-
-    println!("Start  main.");
-
     let mut sta_rx_buffer = [0u8; 1536];
     let mut sta_tx_buffer = [0u8; 1536];
+
     let mut sta_socket = sta_stack.get_socket(&mut sta_rx_buffer, &mut sta_tx_buffer);
 
-    println!("Making HTTP request");
-
-    // let postreq = b"\r\nPOST /rpc/Slide.SetPos HTTP/1.1\r\nAccept: */*\r\nContent-Type: application/json\r\nContent-Length: 11\r\n\r\n{\"pos\":0.8}\r\n";
-    let postreq = b"\r\nPOST /rpc/Slide.GetInfo HTTP/1.1\r\nAccept: */*\r\nContent-Type: application/json\r\nConnection: keep-alive\r\nContent-Length: 0\r\n\r\n";
-
-    let delay = Delay::new();
-    sta_socket.work();
-    println!("Opening socket connection");
-    sta_socket
-        .open(IpAddress::Ipv4(Ipv4Addr::new(192, 168, 68, 104)), 80)
-        .unwrap();
     loop {
-        sta_socket.work();
+        red_led.set_high();
+        controller.start().unwrap();
+        println!("is wifi started: {:?}", controller.is_started());
 
-        println!(
-            "Sending request: {}",
-            core::str::from_utf8(postreq).unwrap()
-        );
-        sta_socket.work();
-        match sta_socket.write(postreq) {
-            Ok(len) => println!("Wrote {len} bytes"),
-            Err(e) => {
-                println!("Error on write {e:?}");
-                break;
-            }
-        }
-        sta_socket.flush().unwrap();
-        sta_socket.work();
+        println!("{:?}", controller.capabilities());
 
-        let deadline = time::Instant::now() + Duration::from_secs(20);
+        println!("wifi_connect {:?}", controller.connect());
+
+        // wait for STA getting an ip address
+        println!("Wait to get an ip address");
         loop {
-            sta_socket.work();
-            let mut buffer = [0u8; 1024];
-            if let Ok(len) = sta_socket.read(&mut buffer) {
-                println!("\n------------ len is {len}  ------------");
-                print!("{}", core::str::from_utf8(&buffer[..len]).unwrap()); // there might be more data, continue reading
-                break;
-            };
-            if time::Instant::now() > deadline {
-                println!("Timeout");
+            sta_stack.work();
+
+            if sta_stack.is_iface_up() {
+                println!("got ip {:?}", sta_stack.get_ip_info());
                 break;
             }
         }
-        println!("Bye1 socket is still open {}", sta_socket.is_open());
-        //        sta_socket.disconnect();
-        delay.delay_millis(10000u32);
-    }
 
-    println!("Done\n");
-    println!();
-    loop {}
+        println!("Start  main work.");
+
+        println!("Making HTTP request");
+
+        // let postreq = b"\r\nPOST /rpc/Slide.SetPos HTTP/1.1\r\nAccept: */*\r\nContent-Type: application/json\r\nContent-Length: 11\r\n\r\n{\"pos\":0.8}\r\n";
+        let postreq = b"\r\nPOST /rpc/Slide.GetInfo HTTP/1.1\r\nAccept: */*\r\nContent-Type: application/json\r\nConnection: keep-alive\r\nContent-Length: 0\r\n\r\n";
+
+        let delay = Delay::new();
+        sta_socket.work();
+        println!("Opening socket connection");
+        sta_socket
+            .open(IpAddress::Ipv4(Ipv4Addr::new(192, 168, 68, 104)), 80)
+            .unwrap();
+        for _i in 1..2 {
+            red_led.set_low();
+            sta_socket.work();
+
+            println!(
+                "Sending request: {}",
+                core::str::from_utf8(postreq).unwrap()
+            );
+            sta_socket.work();
+            match sta_socket.write(postreq) {
+                Ok(len) => println!("Wrote {len} bytes"),
+                Err(e) => {
+                    println!("Error on write {e:?}");
+                    break;
+                }
+            }
+            sta_socket.flush().unwrap();
+            sta_socket.work();
+
+            let deadline = time::Instant::now() + Duration::from_secs(20);
+            loop {
+                sta_socket.work();
+                let mut buffer = [0u8; 1024];
+                if let Ok(len) = sta_socket.read(&mut buffer) {
+                    println!("\n------------ len is {len}  ------------");
+                    print!("{}", core::str::from_utf8(&buffer[..len]).unwrap()); // there might be more data, continue reading
+                    break;
+                };
+                if time::Instant::now() > deadline {
+                    println!("Timeout");
+                    break;
+                }
+            }
+            println!("Bye1 socket is still open {}", sta_socket.is_open());
+            //        sta_socket.disconnect();
+            delay.delay_millis(3000u32);
+            red_led.set_high();
+            delay.delay_millis(500u32);
+        }
+        println!("Done\n");
+        sta_socket.close();
+        sta_socket.disconnect();
+        controller.disconnect();
+        controller.stop();
+
+        red_led.set_low();
+        led6.set_low();
+        println!("sleep/delay");
+        delay.delay_millis(200u32);
+        let timer = TimerWakeupSource::new(core::time::Duration::from_secs(15));
+        rtc.sleep_deep(&[&timer]);
+        println!("woke up");
+    }
 }
 
 fn parse_ip(ip: &str) -> [u8; 4] {
